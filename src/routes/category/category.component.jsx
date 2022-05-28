7d770d8d829f3a0ce603643da7e753a4c8645b72
Monzer Mohammad <dev.monzer@gmail.com>
2022-05-28 13:05:39 +0300

    Update category.component.jsx

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,11 +1,11 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+import { useContext, useState, useEffect, Fragment } from "react";
+import { useParams, Link } from "react-router-dom";
 
-import ProductCard from '../../components/product-card/product-card.component';
+import ProductCard from "../../components/product-card/product-card.component";
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import { CategoriesContext } from "../../contexts/categories.context";
 
-import './category.styles.scss';
+import "./category.styles.scss";
 
 const Category = () => {
   const { category } = useParams();
@@ -18,8 +18,11 @@ const Category = () => {
 
   return (
     <Fragment>
-      <h2 className='category-title'>{category.toUpperCase()}</h2>
-      <div className='category-container'>
+      <h2 className="category-title">
+        {" "}
+        <Link to="/shop">{category.toUpperCase()}</Link>
+      </h2>
+      <div className="category-container">
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
