ed30705a8ee2277a7c6e76917725fd49d68a2dec
Monzer Mohammad <dev.monzer@gmail.com>
2022-05-29 19:51:02 +0300

    started implementing cart based on redux

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import App from './App';
-import { CartProvider } from './contexts/cart.context';
-import { store } from './store/store';
+import App from "./App";
+import { store } from "./store/store";
 
-import './index.scss';
+import "./index.scss";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CartProvider>
-          <App />
-        </CartProvider>
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
