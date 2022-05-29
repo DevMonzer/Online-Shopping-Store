1ba28235d0ba5b13f127928bd64077c111b0ccd6
Monzer Mohammad <dev.monzer@gmail.com>
2022-05-29 13:26:13 +0300

    updated the shop component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import CategoriesPreview from '../categories-preview/categories-preview.component';
-import Category from '../category/category.component';
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { setCategoriesMap } from "../../store/categories/categories.action";
+
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments("categories");
+      dispatch(setCategoriesMap(categoryMap));
+    };
+
+    getCategoriesMap();
+  }, []);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
-      <Route path=':category' element={<Category />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,14 +1,16 @@
 import { combineReducers } from "redux";
 
 import { userReducer } from "./user/user.reducer";
+import { categoriesReducer } from "./categories/categories.reducer";
 
 export const rootReducer = combineReducers({
   user: userReducer,
+  categories: categoriesReducer,
 });
 
 /*
   [2]
 
-  The root reducer file contains all reducers combined together so we later pass it to the redux store.
+  The root reducer file contains all reducers combined together so we later on pass it to the redux store.
 
 */
