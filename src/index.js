153e0ad18f653d4f0e58f0a7952385f3d41a1e0e
Monzer Mohammad <dev.monzer@gmail.com>
2022-05-29 13:32:10 +0300

    completed the redux part of the categories

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 
 import { store } from "./store/store";
@@ -18,11 +17,9 @@ render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,10 +1,13 @@
-import { useContext, Fragment } from 'react';
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
 
-import { CategoriesContext } from '../../contexts/categories.context';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
+import { selectCategoriesMap } from "../../store/categories/categories.selector";
+
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 const CategoriesPreview = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
+  // Getting the categories form the redux store using the useSelector
+  const categoriesMap = useSelector(selectCategoriesMap);
 
   return (
     <Fragment>
