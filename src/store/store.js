c36656b25245ee02d0a4571666e6213e4fcd52bd
Monzer Mohammad <dev.monzer@gmail.com>
2022-05-30 09:48:06 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,21 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import { rootReducer } from './root-reducer';
+import { rootReducer } from "./root-reducer";
 
 const loggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
     return next(action);
   }
+
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
+
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
