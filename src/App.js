3603d21ee3c28748936273140a59e0c07a1ec319
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-01 13:01:58 +0300

    updated the home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from './store/user/user.action';
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
+import { setCurrentUser } from "./store/user/user.action";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
-} from './utils/firebase/firebase.utils';
+} from "./utils/firebase/firebase.utils";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -29,11 +29,11 @@ const App = () => {
   }, [dispatch]);
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
+        <Route path="shop/*" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,12 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
-import Directory from '../../components/directory/directory.component';
+import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
   return (
     <div>
       <Directory />
       <Outlet />
+      This app is built by <a href="https://github.com/DevMonzer">DevMonzer</a>
     </div>
   );
 };
