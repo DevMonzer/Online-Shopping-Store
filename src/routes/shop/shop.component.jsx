c1b2c3bdf282373089292edd70ba3d410f56a5e2
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-05 18:37:05 +0300

    added saga to the shop component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,22 @@
-import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import CategoriesPreview from '../categories-preview/categories-preview.component';
-import Category from '../category/category.component';
-import { fetchCategoriesStartAsync } from '../../store/categories/category.action';
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+import { fetchCategoriesStart } from "../../store/categories/category.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchCategoriesStartAsync());
+    dispatch(fetchCategoriesStart());
   }, []);
 
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
-      <Route path=':category' element={<Category />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,7 +1,7 @@
-import { CATEGORIES_ACTION_TYPES } from './category.types';
-import { createAction } from '../../utils/reducer/reducer.utils';
+import { CATEGORIES_ACTION_TYPES } from "./category.types";
+import { createAction } from "../../utils/reducer/reducer.utils";
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 export const fetchCategoriesStart = () =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
@@ -15,14 +15,14 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
 export const fetchCategoriesFailure = (error) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
 
-export const fetchCategoriesStartAsync = () => {
-  return async (dispatch) => {
-    dispatch(fetchCategoriesStart());
-    try {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
-      dispatch(fetchCategoriesSuccess(categoriesArray));
-    } catch (error) {
-      dispatch(fetchCategoriesFailure(error));
-    }
-  };
-};
+// export const fetchCategoriesStartAsync = () => {
+//   return async (dispatch) => {
+//     dispatch(fetchCategoriesStart());
+//     try {
+//       const categoriesArray = await getCategoriesAndDocuments("categories");
+//       dispatch(fetchCategoriesSuccess(categoriesArray));
+//     } catch (error) {
+//       dispatch(fetchCategoriesFailure(error));
+//     }
+//   };
+// };
