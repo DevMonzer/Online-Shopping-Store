ab1e178288bd9614212eb3cf1b9fb2550fe81cd5
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-05 16:41:19 +0300

    Update categories.reducer.js

--- src/store/categories/categories.reducer.js
+++ src/store/categories/categories.reducer.js
@@ -13,13 +13,19 @@ export const categoriesReducer = (
   const { type, payload } = action;
 
   switch (type) {
-    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-      return { ...state, categories: payload };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+      return { ...state, isLoading: false, categories: payload };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+      return { ...state, isLoading: false, error: payload };
     default:
       return state;
   }
 };
-
 /*
 
   [1]
