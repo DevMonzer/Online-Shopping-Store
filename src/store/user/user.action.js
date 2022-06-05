80179ec5a1ec663850dfe9787220671f2168e72a
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-05 19:30:07 +0300

    added saga sign in functionality

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -1,8 +1,8 @@
 import { USER_ACTION_TYPES } from "./user.types";
 import { createAction } from "../../utils/reducer/reducer.utils";
 
-export const setCurrentUser = (user) =>
-  createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
+// export const setCurrentUser = (user) =>
+//   createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
 
 export const checkUserSession = () =>
   createAction(USER_ACTION_TYPES.CHECK_USER_SESSION);
