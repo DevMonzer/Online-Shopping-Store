13931762ab5a2d9ee07592ea9e7883661fde924c
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-05 17:38:42 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,26 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
-import { rootReducer } from './root-reducer';
+import { rootReducer } from "./root-reducer";
 
 const middleWares = [
-  process.env.NODE_ENV === 'development' && logger,
+  process.env.NODE_ENV === "development" && logger,
   thunk,
 ].filter(Boolean);
 
 const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
+  (process.env.NODE_ENV !== "production" &&
     window &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  blacklist: ['user'],
+  whitelist: ["cart"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
