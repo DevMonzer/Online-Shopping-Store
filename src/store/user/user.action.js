2fa83570108a1e9cf30c326ea74899472c382135
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-05 20:02:39 +0300

    adding sign out functionality using saga

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -26,8 +26,17 @@ export const signUpStart = (email, password, displayName) =>
     displayName,
   });
 
-export const signUpSuccess = (user, additionalDetails) =>
+export const signUpSuccess = ({ user, additionalDetails }) =>
   createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionalDetails });
 
 export const signUpFailed = (error) =>
   createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error);
+
+export const signOutStart = () =>
+  createAction(USER_ACTION_TYPES.SIGN_OUT_START);
+
+export const signOutSuccess = () =>
+  createAction(USER_ACTION_TYPES.SIGN_OUT_SUCCESS);
+
+export const signOutFailed = (error) =>
+  createAction(USER_ACTION_TYPES.SIGN_OUT_FAILED, error);
