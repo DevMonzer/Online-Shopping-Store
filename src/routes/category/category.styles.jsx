8d5987651be30fc16ec4dd5abcee2917bd08dab1
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-06 17:52:50 +0300

    Update category.styles.jsx

--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -1,10 +1,23 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CategoryContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
   row-gap: 50px;
+
+  @media screen and (max-width: 900px) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+  }
+
+  @media screen and (max-width: 550px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 400px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 export const Title = styled.h2`
