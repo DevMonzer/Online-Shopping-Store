de1cc6f7594a5f1d4fb8f96d10a813076ae76470
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-06 17:28:43 +0300

    added css for responsiveness

--- src/components/category-preview/category-preview.styles.jsx
+++ src/components/category-preview/category-preview.styles.jsx
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const CategoryPreviewContainer = styled.div`
   display: flex;
@@ -18,4 +18,10 @@ export const Preview = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
+
+  @media screen and (max-width: 900px) {
+    grid-template-columns: repeat(3, 1fr);
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
 `;
