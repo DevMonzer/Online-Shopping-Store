838d630dd23f3bab67130ef8b41a816dc59a7e6e
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-06 19:38:17 +0300

    updated package.json

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -29,6 +29,15 @@ const firebaseConfig = {
   appId: "1:781141198881:web:11e2a65056cdc75fedfb60",
 };
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_API_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_API_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_API_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_API_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_API_APP_ID,
+// };
+
 const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
