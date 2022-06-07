0877c1fa7c224393f4a9772b8fab7e8ebe0866b9
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-07 21:00:15 +0300

    added css responsiveness to the auth component or page

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,12 +1,13 @@
-import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
-import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-import { AuthenticationContainer } from './authentication.styles';
+import { AuthenticationContainer, MarginBottom } from "./authentication.styles";
 
 const Authentication = () => {
   return (
     <AuthenticationContainer>
       <SignInForm />
+      <MarginBottom />
       <SignUpForm />
     </AuthenticationContainer>
   );
--- src/routes/authentication/authentication.styles.jsx
+++ src/routes/authentication/authentication.styles.jsx
@@ -1,8 +1,21 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const AuthenticationContainer = styled.div`
   display: flex;
-  width: 900px;
+  width: 90%;
   justify-content: space-between;
   margin: 30px auto;
+
+  @media screen and (max-width: 900px) {
+    width: 100%;
+    align-items: center;
+    flex-direction: column;
+    margin: 0;
+  }
+`;
+
+export const MarginBottom = styled.span`
+  @media screen and (max-width: 800px) {
+    margin: 30px 0;
+  }
 `;
