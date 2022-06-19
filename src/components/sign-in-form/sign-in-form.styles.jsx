77581d432813c552f99fc6ad4ada74442a75f5e3
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-19 10:12:34 +0300

    Update sign-in-form.styles.jsx

--- src/components/sign-in-form/sign-in-form.styles.jsx
+++ src/components/sign-in-form/sign-in-form.styles.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const SignInContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 300px;
+  width: 460px;
 
   margin: 0 auto;
 
