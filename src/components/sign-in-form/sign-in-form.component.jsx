61665e95d0bcf0e54bbabf2f22630d71f4bbcb8c
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-19 13:11:59 +0300

    Update sign-in-form.component.jsx

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -44,7 +44,7 @@ const SignInForm = () => {
       dispatch(emailSignInStart(email, password));
       resetFormFields();
       // redirect to /shop if user signed in successfully
-      navigate("/shop");
+      // navigate("/shop");
     } catch (error) {
       alert("user sign in failed", error);
     }
