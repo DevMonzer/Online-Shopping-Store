43c29eef0160bcb2ecb6e862230f4fe516dbfd0a
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-19 10:20:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
+import SignUp from "./components/sign-up/sign-up-form.component";
+
 import { checkUserSession } from "./store/user/user.action";
 
 import { GlobalStyle } from "./global.styles";
