314c24b2f33a5f00ccb0d5024a368edd2405ca7a
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-19 13:07:37 +0300

    Update authentication.component.jsx

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,7 +1,7 @@
-// import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-import { AuthenticationContainer } from "./authentication.styles";
+import { AuthenticationContainer, MarginBottom } from "./authentication.styles";
 
 const Authentication = () => {
   return (
