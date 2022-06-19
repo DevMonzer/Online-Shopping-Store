594436ac1cc790b8cb7b4d023e48973c2a0b9b7a
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-19 10:36:28 +0300

    Update sign-up-form.styles.jsx

--- src/components/sign-up-form/sign-up-form.styles.jsx
+++ src/components/sign-up-form/sign-up-form.styles.jsx
@@ -17,4 +17,8 @@ export const Margin = styled.div`
 export const LinkStyle = styled.div`
   min-width: 165px;
   width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 0 35px 0 35px;
 `;
