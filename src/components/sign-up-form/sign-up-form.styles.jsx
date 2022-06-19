c9035d8ee629404073fe0b0d415b2251914c4d8f
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-19 10:42:11 +0300

    Update sign-up-form.styles.jsx

--- src/components/sign-up-form/sign-up-form.styles.jsx
+++ src/components/sign-up-form/sign-up-form.styles.jsx
@@ -3,7 +3,9 @@ import styled from "styled-components";
 export const SignUpContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 380px;
+  width: 460px;
+
+  margin: 0 auto;
 
   h2 {
     margin: 10px 0;
