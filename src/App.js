9830a23c88e7b085778a7f3359064fdf1eef6eb4
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-19 13:10:53 +0300

    deleted auth component and updated routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
+import SignIn from "./components/sign-in-form/sign-in-form.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
 import SignUp from "./components/sign-up-form/sign-up-form.component";
@@ -28,7 +28,7 @@ const App = () => {
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
           <Route path="shop/*" element={<Shop />} />
-          <Route path="/sign-in" element={<Authentication />} />
+          <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="checkout" element={<Checkout />} />
         </Route>
