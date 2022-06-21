c0bd594362293c4a660fc580382c6053e4bd0606
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-21 17:26:53 +0300

    Update sign-up-form.component.jsx

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import FormInput from "../form-input/form-input.component";
