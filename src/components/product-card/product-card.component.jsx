c3aa16aac0aa89c38a59fee7b64ac47dff78d721
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-22 10:17:30 +0300

    Update product-card.component.jsx

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -13,9 +13,6 @@ import {
 } from "./product-card.styles";
 
 const ProductCard = ({ product }) => {
-  // Getting the currentUser state
-  const currentUser = useSelector(selectCurrentUser);
-
   const { name, price, imageUrl } = product;
   const dispatch = useDispatch();
   const cartItems = useSelector(selectCartItems);
