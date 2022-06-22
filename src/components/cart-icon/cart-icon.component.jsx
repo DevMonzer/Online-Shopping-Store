5553d5016c6cbc595e2fbc695f1679d80f68839d
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-22 10:23:10 +0300

    Update cart-icon.component.jsx

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,6 +6,8 @@ import {
 } from "../../store/cart/cart.selector";
 import { setIsCartOpen } from "../../store/cart/cart.action.js";
 
+import { selectCurrentUser } from "../../store/user/user.selector";
+
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import { CartIconContainer, ItemCount } from "./cart-icon.styles";
