3b3ccb7eefda1509d2708a2e51de3c141da2161b
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-22 10:18:07 +0300

    Update product-card.component.jsx

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,6 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
 
 import { selectCartItems } from "../../store/cart/cart.selector";
+
+import { selectCurrentUser } from "../../store/user/user.selector";
+
 import { addItemToCart } from "../../store/cart/cart.action";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
