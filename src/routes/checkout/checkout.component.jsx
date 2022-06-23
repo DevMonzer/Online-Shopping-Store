9f07f48fc80115ab84573ff5aaad3ce9f63ca9ad
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-23 09:36:11 +0300

    Update checkout.component.jsx

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -43,7 +43,7 @@ const Checkout = () => {
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <Total>Total: {cartTotal}</Total>
+      <Total>Total: ${cartTotal}</Total>
 
       <PaymentForm />
     </CheckoutContainer>
