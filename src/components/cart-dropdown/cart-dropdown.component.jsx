72f9c07ce824d1091060779deb6047adbdfc3794
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-23 09:27:45 +0300

    Update cart-dropdown.component.jsx

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,10 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-import Button from "../button/button.component";
-import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../store/cart/cart.selector";
 import { selectCurrentUser } from "../../store/user/user.selector";
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import {
   CartDropdownContainer,
