8bca09c38d2e8c38fde70e89cc4dbf188fe1dd92
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-23 12:49:46 +0300

    Update directory-item.styles.jsx

--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -36,7 +36,7 @@ export const Body = styled.div`
 
 export const DirectoryItemContainer = styled.div`
   min-width: 30%;
-  height: 240px;
+  height: 280px;
   flex: 1 1 auto;
   display: flex;
   align-items: center;
