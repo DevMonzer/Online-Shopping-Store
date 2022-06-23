fda7d56c219ad95ed436f234fb22acfb3e4726cb
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-23 09:37:10 +0300

    Update spinner.component.jsx

--- src/components/spinner/spinner.component.jsx
+++ src/components/spinner/spinner.component.jsx
@@ -3,7 +3,6 @@ import { SpinnerContainer, SpinnerOverlay } from "./spinner.styles";
 const Spinner = () => (
   <SpinnerOverlay>
     <SpinnerContainer />
-    <SpinnerContainer />
   </SpinnerOverlay>
 );
 
