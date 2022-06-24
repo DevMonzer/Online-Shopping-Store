f92898e3fe4f31570c2fb6db6f31e0706b859e0e
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-24 18:08:14 +0300

    Update user.action.js

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -22,6 +22,12 @@ export const googleSignInStart = () =>
 export const googleSignInWithRedirectStart = () =>
   createAction(USER_ACTION_TYPES.GOOGLE_REDIRECT_SIGN_IN_START);
 
+export const facebookSignInStart = () =>
+  createAction(USER_ACTION_TYPES.FACEBOOK_SIGN_IN_START);
+
+export const facebookSignInWithRedirectStart = () =>
+  createAction(USER_ACTION_TYPES.FACEBOOK_REDIRECT_SIGN_IN_START);
+
 export const emailSignInStart = (email, password) =>
   createAction(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, { email, password });
 
