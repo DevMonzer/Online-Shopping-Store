5b74afb1cd0698c513acaccb9d782a22db22ee0d
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-02 10:51:42 +0300

    Update card-details.jsx

--- src/components/card-details/card-details.jsx
+++ src/components/card-details/card-details.jsx
@@ -18,33 +18,31 @@ import {
 
 import Spinner from "../spinner/spinner.component";
 
-
 const Card = () => {
-    const { cardId } = useParams();
-    const { categoriesMap } = useContext(CategoriesContext);
-    const [productCard, setProductCard] = useState([]);
-  
-    if (window.location.pathname.includes("/shop/hats/")) {
-      var data = categoriesMap["hats"];
-      var route = "/shop/hats";
-    } else if (window.location.pathname.includes("/shop/jackets/")) {
-      var data = categoriesMap["jackets"];
-      var route = "/shop/jackets";
-    } else if (window.location.pathname.includes("/shop/sneakers/")) {
-      var data = categoriesMap["sneakers"];
-      var route = "/shop/sneakers";
-    } else if (window.location.pathname.includes("/shop/womens/")) {
-      var data = categoriesMap["womens"];
-      var route = "/shop/womens";
-    } else if (window.location.pathname.includes("/shop/mens/")) {
-      var data = categoriesMap["mens"];
-      var route = "/shop/mens";
-    } else if (window.location.pathname.includes("/shop/kids/")) {
-      var data = categoriesMap["kids"];
-      var route = "/shop/kids";
-    }
+  const { cardId } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [productCard, setProductCard] = useState([]);
 
-    
+  if (window.location.pathname.includes("/shop/hats/")) {
+    var data = categoriesMap["hats"];
+    var route = "/shop/hats";
+  } else if (window.location.pathname.includes("/shop/jackets/")) {
+    var data = categoriesMap["jackets"];
+    var route = "/shop/jackets";
+  } else if (window.location.pathname.includes("/shop/sneakers/")) {
+    var data = categoriesMap["sneakers"];
+    var route = "/shop/sneakers";
+  } else if (window.location.pathname.includes("/shop/womens/")) {
+    var data = categoriesMap["womens"];
+    var route = "/shop/womens";
+  } else if (window.location.pathname.includes("/shop/mens/")) {
+    var data = categoriesMap["mens"];
+    var route = "/shop/mens";
+  } else if (window.location.pathname.includes("/shop/kids/")) {
+    var data = categoriesMap["kids"];
+    var route = "/shop/kids";
+  }
+
   useEffect(() => {
     if (data) {
       const product = data.filter((product) => product.id == cardId);
@@ -65,3 +63,39 @@ const Card = () => {
 
   const logInHandler = () =>
     alert("Please log in to your account before adding products");
+
+  return (
+    <Fragment>
+      <div className="productCardContainer">
+        <img
+          className="productCardImage"
+          src={productCard.imageUrl}
+          alt={`${productCard.name}`}
+        />
+        <div className="productCardDetails">
+          <p>{productCard.name}</p>
+          <p>${productCard.price}</p>
+          {currentUser ? (
+            <Button
+              buttonType={BUTTON_TYPE_CLASSES.inverted}
+              onClick={addProductToCart}
+            >
+              Add to card
+            </Button>
+          ) : (
+            <Button onClick={logInHandler}>Log in FIRST</Button>
+          )}
+          <br />
+          <Button
+            buttonType={BUTTON_TYPE_CLASSES.inverted}
+            onClick={onNavigateHandler}
+          >
+            Go Back
+          </Button>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Card;
