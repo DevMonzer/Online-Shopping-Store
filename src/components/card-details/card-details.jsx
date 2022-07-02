e707d25088a9eb7544e668c63a64aa9ec883e1aa
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-02 10:51:17 +0300

    Update card-details.jsx

--- src/components/card-details/card-details.jsx
+++ src/components/card-details/card-details.jsx
@@ -17,3 +17,29 @@ import {
 } from "./card-details.styles.scss";
 
 import Spinner from "../spinner/spinner.component";
+
+
+const Card = () => {
+    const { cardId } = useParams();
+    const { categoriesMap } = useContext(CategoriesContext);
+    const [productCard, setProductCard] = useState([]);
+  
+    if (window.location.pathname.includes("/shop/hats/")) {
+      var data = categoriesMap["hats"];
+      var route = "/shop/hats";
+    } else if (window.location.pathname.includes("/shop/jackets/")) {
+      var data = categoriesMap["jackets"];
+      var route = "/shop/jackets";
+    } else if (window.location.pathname.includes("/shop/sneakers/")) {
+      var data = categoriesMap["sneakers"];
+      var route = "/shop/sneakers";
+    } else if (window.location.pathname.includes("/shop/womens/")) {
+      var data = categoriesMap["womens"];
+      var route = "/shop/womens";
+    } else if (window.location.pathname.includes("/shop/mens/")) {
+      var data = categoriesMap["mens"];
+      var route = "/shop/mens";
+    } else if (window.location.pathname.includes("/shop/kids/")) {
+      var data = categoriesMap["kids"];
+      var route = "/shop/kids";
+    }
