9d87ebe309a8af28a0829f2bea18cb4b7b039d5c
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-05 13:42:14 +0300

    update buttons  style

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -4,6 +4,7 @@ import {
   FacebookSignInButton,
   InvertedButton,
   LoadingSpinner,
+  YellowButton,
 } from "./button.styles";
 
 export const BUTTON_TYPE_CLASSES = {
@@ -11,12 +12,14 @@ export const BUTTON_TYPE_CLASSES = {
   google: "google-sign-in",
   facebook: "facebook-sign-in",
   inverted: "inverted",
+  yellow: "YellowButton",
 };
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
   ({
     [BUTTON_TYPE_CLASSES.base]: BaseButton,
     [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.yellow]: YellowButton,
     [BUTTON_TYPE_CLASSES.facebook]: FacebookSignInButton,
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
