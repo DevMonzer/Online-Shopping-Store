ef49ffd26c0d2aa4f2d368674a636046fdb10206
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-07 13:28:35 +0300

    Update cart-dropdown.styles.jsx

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -31,15 +31,6 @@ export const EmptyMessage = styled.span`
   margin: 50px auto;
 `;
 
-// export const CartItems = styled.div`
-//   // height: 240px;
-//   display: flex;
-//   flex-direction: column;
-//   // overflow-y: scroll;
-//   overflow: hidden;
-//   overflow-y: auto;
-// `;
-
 export const CartItems = styled.div`
   width: 100%;
   height: 100%;
