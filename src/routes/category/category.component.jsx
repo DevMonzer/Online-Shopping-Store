f89ca4421ad225246bfa97010a04d40be9497464
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-15 16:42:08 +0300

    Update category.component.jsx

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -14,7 +14,7 @@ import {
 
 import { CategoryContainer, Title, Center } from "./category.styles";
 
-import "./category.styles.css";
+import "./category.styles.scss";
 
 // Nested Categories Pages inside the Shop Page
 const Category = () => {
