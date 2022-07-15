697c141530eaa505b9656a1c862a6e11fd0f1352
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-15 16:20:26 +0300

    Update category-preview.component.jsx

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,3 +1,13 @@
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from "../../store/categories/category.selector";
+
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+import Spinner from "../../components/spinner/spinner.component";
 import ProductCard from "../product-card/product-card.component";
 
 import {
