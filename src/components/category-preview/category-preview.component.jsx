356c98bae1a6e33baf9c886581885e841ee55113
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-15 16:32:48 +0300

    Update category-preview.component.jsx

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -6,7 +6,6 @@ import {
   selectCategoriesIsLoading,
 } from "../../store/categories/category.selector";
 
-import CategoryPreview from "../../components/category-preview/category-preview.component";
 import Spinner from "../../components/spinner/spinner.component";
 
 import ProductCard from "../../components/product-card/product-card.component";
