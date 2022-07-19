665c2a36fdae078ae334336f4a45fd009b47594f
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-19 12:51:36 +0300

    Update slider.component.jsx

--- src/components/slider/slider.component.jsx
+++ src/components/slider/slider.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import { Typography } from "@mui/material";
