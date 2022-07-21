1a9d89c65e884e55f5f4de56a938ab3162fe62a7
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-21 19:56:08 +0300

    Update navBar.component.jsx

--- src/components/shop-directory/navBar/navBar.component.jsx
+++ src/components/shop-directory/navBar/navBar.component.jsx
@@ -83,5 +83,20 @@ const ResponsiveAppBar = () => {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  }));
 };
 export default ResponsiveAppBar;
