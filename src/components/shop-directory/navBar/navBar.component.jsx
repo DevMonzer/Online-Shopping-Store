345fd994b1b202c051b16e0d41a4484cdf4a63ec
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-21 19:56:17 +0300

    Update navBar.component.jsx

--- src/components/shop-directory/navBar/navBar.component.jsx
+++ src/components/shop-directory/navBar/navBar.component.jsx
@@ -98,5 +98,15 @@ const ResponsiveAppBar = () => {
       width: "auto",
     },
   }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
 };
 export default ResponsiveAppBar;
