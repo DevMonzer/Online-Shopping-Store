c25080b25ac100fa40f15fde7d68f20f17583add
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-21 19:54:46 +0300

    Update navBar.component.jsx

--- src/components/shop-directory/navBar/navBar.component.jsx
+++ src/components/shop-directory/navBar/navBar.component.jsx
@@ -15,3 +15,20 @@ import AdbIcon from "@mui/icons-material/Adb";
 import { styled, alpha } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import SearchIcon from "@mui/icons-material/Search";
+
+import { Link, useNavigate } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
+import SearchBar from "../../components/search-bar/SearchBar";
+
+import "./navBar.styles.scss";
