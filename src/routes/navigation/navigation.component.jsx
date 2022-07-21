71b6ad44287c7dc5f1715696052294ae65d277b8
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-21 20:18:33 +0300

    Update navigation.component.jsx

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,79 +1,11 @@
-import { Fragment, useState, useEffect } from "react";
+import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-
-import CartIcon from "../../components/cart-icon/cart-icon.component";
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import SearchBar from "../../components/search-bar/SearchBar";
-
-import { selectIsCartOpen } from "../../store/cart/cart.selector";
-import { selectCurrentUser } from "../../store/user/user.selector";
-import { signOutStart } from "../../store/user/user.action";
-import { selectCategoriesMap } from "../../store/categories/category.selector";
-import { fetchCategoriesStart } from "../../store/categories/category.action";
-
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 import NavBar from "../../components/navBar/navBar.component";
 
-import {
-  NavigationContainer,
-  NavLinks,
-  NavLink,
-  LogoContainer,
-  Container,
-  SearchContainer,
-} from "./navigation.styles";
-
 const Navigation = () => {
-  const dispatch = useDispatch();
-  const currentUser = useSelector(selectCurrentUser);
-  const isCartOpen = useSelector(selectIsCartOpen);
-
-  const signOutUser = () => dispatch(signOutStart());
-
-  //////////////////// SearchBar Code ////////////////////
-
-  useEffect(() => {
-    dispatch(fetchCategoriesStart());
-  }, []);
-
-  const categoriesMap = useSelector(selectCategoriesMap);
-
-  let allProducts = [];
-  const products = Object.values(categoriesMap).map((product) => {
-    allProducts.push(...product);
-  });
-  // console.log(allProducts);
-
-  ////////////////////////////////////////////////////////
-
   return (
     <Fragment>
-      {/* <Container>
-        <SearchContainer>
-          {products && products.length ? (
-            <SearchBar placeholder="Search " data={allProducts} />
-          ) : null}
-        </SearchContainer>
-        <NavigationContainer>
-          <LogoContainer to="/">
-            <CrwnLogo className="logo" />
-          </LogoContainer>
-          <NavLinks>
-            <NavLink to="/shop">SHOP</NavLink>
-            {currentUser ? (
-              <NavLink as="span" onClick={signOutUser}>
-                SIGN OUT
-              </NavLink>
-            ) : (
-              <NavLink to="/sign-in">SIGN IN</NavLink>
-            )}
-            <CartIcon />
-          </NavLinks>
-          {isCartOpen && <CartDropdown />}
-        </NavigationContainer>
-      </Container> */}
       <NavBar />
       <Outlet />
     </Fragment>
