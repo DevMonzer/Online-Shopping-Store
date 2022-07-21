41a6d08e796d31569c8a4359cf39cf7207d2f0dd
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-21 20:15:36 +0300

    add contact us route

---
+++ src/routes/contact-us/contact-us.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <div style={{ textAlign: "center", fontSize: "30px", color: "red" }}>
+      This route is not implemented yet.
+      <p style={{ color: "green" }}>Go Back</p>
+    </div>
+  );
+};
+
+export default ContactUs;
