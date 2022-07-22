11a5f3d2fa96910d20699bbb2cc16957c784fc8e
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-22 17:02:57 +0300

    Update footer.component.jsx

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
