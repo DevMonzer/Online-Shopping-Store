bdebe8c1ef5efa86c41daa217678e0197bdeb440
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-22 17:05:14 +0300

    Update footer.component.jsx

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -68,25 +68,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-// <!DOCTYPE html>
-// <html>
-
-// <head>
-//     <meta charset="utf-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Untitled</title>
-//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
-//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-//     <link rel="stylesheet" href="assets/css/style.css">
-// </head>
-
-// <body>
-
-//     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-//     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-// </body>
-
-// </html>
-
-// <!-- Credit to https://epicbootstrap.com/snippets/footer-dark --></link>
