968cfdae798a6f4ac7d3eb42327d41022ecdc4df
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-22 13:17:40 +0300

    Update global.styles.js

--- src/global.styles.js
+++ src/global.styles.js
@@ -2,11 +2,11 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 
-* {
-	padding: 0;
-	margin: 0;
-	box-sizing: border-box;
-}
+	* {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}	
 	body {
 		font-family: 'Open Sans Condensed';
 		padding: 0;
