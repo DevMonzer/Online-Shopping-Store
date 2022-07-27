bd21bbff03f676e0d2270249bafb2d7173863948
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-27 17:23:23 +0300

    Update card-details.jsx

--- src/components/card-details/card-details.jsx
+++ src/components/card-details/card-details.jsx
@@ -50,47 +50,46 @@ const Card = () => {
       {!allProducts?.length ? (
         <Loader />
       ) : (
-        <Loader />
-        // <>
-        //   <div className="productCardContainer">
-        //     <img
-        //       className="productCardImage"
-        //       src={productCard.imageUrl}
-        //       alt={`${productCard.name}`}
-        //     />
-        //     <div className="productCardDetails">
-        //       <p>
-        //         This is&nbsp;
-        //         <span className="productCardName">{productCard.name}</span>
-        //         <br /> and you can get only for
-        //       </p>
-        //       <p>${productCard.price}</p>
-        //       {currentUser ? (
-        //         <Button
-        //           buttonType={BUTTON_TYPE_CLASSES.facebook}
-        //           onClick={addProductToCart}
-        //         >
-        //           Add to card
-        //         </Button>
-        //       ) : (
-        //         <Button
-        //           buttonType={BUTTON_TYPE_CLASSES.facebook}
-        //           onClick={logInHandler}
-        //         >
-        //           Log in FIRST
-        //         </Button>
-        //       )}
-        //       <br />
-        //       <Button
-        //         buttonType={BUTTON_TYPE_CLASSES.google}
-        //         onClick={() => navigate(-1)}
-        //       >
-        //         Go Back
-        //       </Button>
-        //     </div>
-        //   </div>
-        //   <Slider title="Top Products" allProducts={allProducts} />
-        // </>
+        <>
+          <div className="productCardContainer">
+            <img
+              className="productCardImage"
+              src={productCard.imageUrl}
+              alt={`${productCard.name}`}
+            />
+            <div className="productCardDetails">
+              <p>
+                This is&nbsp;
+                <span className="productCardName">{productCard.name}</span>
+                <br /> and you can get only for
+              </p>
+              <p>${productCard.price}</p>
+              {currentUser ? (
+                <Button
+                  buttonType={BUTTON_TYPE_CLASSES.facebook}
+                  onClick={addProductToCart}
+                >
+                  Add to card
+                </Button>
+              ) : (
+                <Button
+                  buttonType={BUTTON_TYPE_CLASSES.facebook}
+                  onClick={logInHandler}
+                >
+                  Log in FIRST
+                </Button>
+              )}
+              <br />
+              <Button
+                buttonType={BUTTON_TYPE_CLASSES.google}
+                onClick={() => navigate(-1)}
+              >
+                Go Back
+              </Button>
+            </div>
+          </div>
+          <Slider title="Top Products" allProducts={allProducts} />
+        </>
       )}
     </Fragment>
   );
