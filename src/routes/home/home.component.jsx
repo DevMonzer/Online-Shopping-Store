27fe694c6a649240e08bb6778e5bb5918e4ed54f
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-28 13:46:01 +0300

    Update home.component.jsx

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 
 import HomeDirectory from "../../components/home-directory/home-directory.component";
-import RecipeReviewCard from "../../components/shopping-card/shopping-card.component";
 
 const categories = [
   {
