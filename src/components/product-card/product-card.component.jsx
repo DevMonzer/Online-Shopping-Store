90dcf65e2e796ee0dc66f7de37a734e5fd8e4e1a
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-28 13:28:35 +0300

    Update product-card.component.jsx

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -7,8 +7,6 @@ import { addItemToCart } from "../../store/cart/cart.action";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
-import Card from "../card/card";
-
 import {
   ProductCartContainer,
   Footer,
