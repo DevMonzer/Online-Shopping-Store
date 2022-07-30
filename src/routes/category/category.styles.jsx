2b47278364e0ac1248f636a52e755683b75d8bf6
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-30 20:16:54 +0300

    Update category.styles.jsx

--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -6,6 +6,10 @@ export const CategoryContainer = styled.div`
   column-gap: 20px;
   row-gap: 50px;
 
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
   @media screen and (max-width: 900px) {
     grid-template-columns: repeat(3, 1fr);
     gap: 20px;
