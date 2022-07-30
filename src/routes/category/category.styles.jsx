0c876a1fd5dfd4e6d7eaef55dbccf8bad9eecbdc
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-30 20:16:47 +0300

    Update category.styles.jsx

--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const CategoryContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(5, 1fr);
   column-gap: 20px;
   row-gap: 50px;
 
