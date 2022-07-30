6dde25ca5eed3b447997f66510077b592501f1b2
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-30 20:15:33 +0300

    Update shop-directory.styles.jsx

--- src/components/shop-directory/shop-directory.styles.jsx
+++ src/components/shop-directory/shop-directory.styles.jsx
@@ -28,7 +28,7 @@ export const Preview = styled.div`
   grid-template-columns: repeat(5, 1fr);
   column-gap: 20px;
 
-  @media screen and (max-width: 1100px) {
+  @media screen and (max-width: 1200px) {
     grid-template-columns: repeat(4, 1fr);
   }
 
