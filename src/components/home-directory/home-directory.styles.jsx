4200699eb6aa6fa585bf77666fc5217cfdcd5f5e
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-30 20:18:55 +0300

    Update home-directory.styles.jsx

--- src/components/home-directory/home-directory.styles.jsx
+++ src/components/home-directory/home-directory.styles.jsx
@@ -45,7 +45,7 @@ export const Body = styled.div`
 
 export const DirectoryItemContainer = styled.div`
   min-width: 30%;
-  height: 280px;
+  height: 320px;
   flex: 1 1 auto;
   display: flex;
   align-items: center;
