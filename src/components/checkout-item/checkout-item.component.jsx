19554350064af4df1a49599dac2314231de3db89
Monzer Mohammad <dev.monzer@gmail.com>
2022-08-01 13:31:42 +0300

    Update checkout-item.component.jsx

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -45,7 +45,7 @@ const CheckoutItem = ({ cartItem }) => {
 
   return (
     <CheckoutItemContainer>
-      <ImageContainer>
+      <ImageContainer onClick={() => handleNavigate(route)}>
         <img src={imageUrl} alt={`${name}`} />
       </ImageContainer>
       <BaseSpan> {name} </BaseSpan>
