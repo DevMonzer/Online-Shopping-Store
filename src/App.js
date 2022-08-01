65028c7e357f4ae7968f29cfc6ca70417af3c99a
Monzer Mohammad <dev.monzer@gmail.com>
2022-08-01 13:08:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
