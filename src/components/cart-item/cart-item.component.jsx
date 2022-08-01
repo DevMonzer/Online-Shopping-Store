f40034b501084fb1e433423a350398498dbf47c7
Monzer Mohammad <dev.monzer@gmail.com>
2022-08-01 13:19:56 +0300

    Update cart-item.component.jsx

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -3,8 +3,8 @@ import { CartItemContainer, ItemDetails } from "./cart-item.styles";
 
 const CartItem = ({ cartItem }) => {
   const navigate = useNavigate();
-
   const { name, imageUrl, price, quantity, route } = cartItem;
+  const handleNavigate = navigate(route);
   return (
     <CartItemContainer>
       <img src={imageUrl} alt={`${name}`} />
