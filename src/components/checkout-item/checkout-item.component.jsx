153283ac8d43feadc9f08ba60f8c8873dd0f85c2
Monzer Mohammad <dev.monzer@gmail.com>
2022-08-01 13:30:43 +0300

    Update checkout-item.component.jsx

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import { selectCartItems } from "../../store/cart/cart.selector";
 import {
