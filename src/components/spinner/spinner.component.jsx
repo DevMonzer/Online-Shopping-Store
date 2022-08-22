d9c95de4d77a3d6f9d090d8d10f2b6471a95ce1c
Monzer Mohammad <dev.monzer@gmail.com>
2022-08-22 13:28:18 +0300

    Update spinner.component.jsx

--- src/components/spinner/spinner.component.jsx
+++ src/components/spinner/spinner.component.jsx
@@ -1,24 +1,22 @@
 import "./spinner.css";
 
 const Spinner = () => (
-  <>
-    <div className="loadingio-spinner-spinner-r0dzq8q9gui">
-      <div className="ldio-2rt7av59393">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+  <div className="loadingio-spinner-spinner-r0dzq8q9gui">
+    <div className="ldio-2rt7av59393">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
-  </>
+  </div>
 );
 
 export default Spinner;
