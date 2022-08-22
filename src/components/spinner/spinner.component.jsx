78789e63a7352c549d9a1a17ee8d7adfac54b068
Monzer Mohammad <dev.monzer@gmail.com>
2022-08-22 13:26:54 +0300

    update the spinner

--- src/components/spinner/spinner.component.jsx
+++ src/components/spinner/spinner.component.jsx
@@ -1,8 +1,25 @@
 import { SpinnerContainer, SpinnerOverlay } from "./spinner.styles";
+import "./spinner.css";
 
 const Spinner = () => (
   <SpinnerOverlay>
-    <SpinnerContainer />
+    {/* <SpinnerContainer /> */}
+    <div className="loadingio-spinner-spinner-r0dzq8q9gui">
+      <div className="ldio-2rt7av59393">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   </SpinnerOverlay>
 );
 
