001f0eb995d48b3dc8ca9213f2b831489ab3355f
Monzer Mohammad <dev.monzer@gmail.com>
2022-08-22 13:40:10 +0300

    Update navigation.component.jsx

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,25 @@
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 
+import { selectCategoriesIsLoading } from "../../store/categories/category.selector";
+
 import NavBar from "../../components/navBar/navBar.component";
 import Footer from "../../components/footer/footer.component";
 
 const Navigation = () => {
+  const categoriesMap = useSelector(selectCategoriesMap);
+
   return (
     <Fragment>
-      <NavBar />
-      <Outlet />
-      <Footer />
+      {!isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <NavBar />
+          <Outlet />
+          <Footer />
+        </>
+      )}
     </Fragment>
   );
 };
