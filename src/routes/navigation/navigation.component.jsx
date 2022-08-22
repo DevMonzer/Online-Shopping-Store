512c5c9c1425f5362a4e85269c8a0ae7865f5a33
Monzer Mohammad <dev.monzer@gmail.com>
2022-08-22 13:45:59 +0300

    Update navigation.component.jsx

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,28 +1,16 @@
 import { Fragment } from "react";
-import { useSelector } from "react-redux";
 
 import { Outlet } from "react-router-dom";
-import Spinner from "../../components/spinner/spinner.component";
-
-import { selectCategoriesIsLoading } from "../../store/categories/category.selector";
 
 import NavBar from "../../components/navBar/navBar.component";
 import Footer from "../../components/footer/footer.component";
 
 const Navigation = () => {
-  const isLoading = useSelector(selectCategoriesIsLoading);
-
   return (
     <Fragment>
-      {!isLoading ? (
-        <Spinner />
-      ) : (
-        <>
-          <NavBar />
-          <Outlet />
-          <Footer />
-        </>
-      )}
+      <NavBar />
+      <Outlet />
+      <Footer />
     </Fragment>
   );
 };
