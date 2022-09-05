eb6dac1e5037d2e71172c2e1e43c4496b4aa54ec
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-05 09:28:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import ContactUs from "./components/contact-us/contact-us.component";
 import SignIn from "./components/sign-in-form/sign-in-form.component";
 import SignUp from "./components/sign-up-form/sign-up-form.component";
 
-import { checkUserSession } from "./store/user/user.action";
+// import { checkUserSession } from "./store/user/user.action";
 
 const App = () => {
   // Checking the user session
